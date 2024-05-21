4dcb66361d3882d93bf29691eb758f81b707205b
Dustin Conlon <83980826+VoltaicGRiD-Canon@users.noreply.github.com>
2024-05-21 12:01:15 -0600

    Update hop.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,31 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Hop.nvim
+map({ "n", "i" }, "ghw", "<cmd>:HopWord<cr>", { noremap = true })
+map({ "n", "i" }, "ghj", "<cmd>:HopLineStart<cr>", { noremap = true })
+map({ "n", "i" }, "gh/", "<cmd>:HopPattern<cr>", { noremap = true })
+map({ "n", "i" }, "ghs", "<cmd>:HopChar2<cr>", { noremap = true })
+
+-- Open Neotree
+map("n", "<C-f>", ":Neotree<CR>", {})
+
+-- Exit insert mode and save file 'fds'
+map({ "i", "n" }, "fds", function()
+  vim.cmd("write")
+  vim.cmd("stopinsert")
+  print("Saved file: " .. vim.fn.expand("%:p"))
+end, { noremap = true, silent = true })
+
+-- Exit insert mode and do NOT save file 'fd'
+map("i", "fd", function()
+  vim.cmd("stopinsert")
+  print("Exited insert mode without saving")
+end, { noremap = true, silent = true })
+
+require("which-key").register({
+  f = "which_key_ignore",
+}, { mode = "i" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,24 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.scrolloff = 10
+opt.colorcolumn = { 80, 120 }
+opt.list = false
+
+vim.o.shell = "powershell"
+
+vim.o.shellcmdflag =
+  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+
+vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+
+vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+
+vim.o.shellquote = ""
+
+vim.o.shellxquote = ""
